19696c0f557064efc1245d7a8052d0643db5cb4b
tensialee <clee134@bu.edu>
2020-01-29 14:13:28 -0500

    update translate

--- join_data.R
+++ join_data.R
@@ -1,11 +1,12 @@
 library(tidyverse)
 
-setwd("~/BA888Spring")
+setwd("~/Desktop/BA 888/888/")
 
 sellers <- read_csv("olist_sellers_dataset.csv")
 items <- read_csv("olist_order_items_dataset.csv")
 cd <- read_csv("olist_closed_deals_dataset.csv")
 mql <- read_csv("olist_marketing_qualified_leads_dataset.csv")
+trsanlatedReview <- read_csv("Translated_reviews - order_review_translated.csv")
 
 mf <- merge(cd, mql, by="mql_id")
 BR <- merge(sellers, items, by="seller_id")
@@ -13,3 +14,5 @@ combined <- merge(mf, BR, by="seller_id")
 
 final <- combined %>% 
   select(-has_company,-has_gtin,-average_stock,-declared_product_catalog_size)
+
+View(final)
