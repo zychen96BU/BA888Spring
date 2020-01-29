0c0c9b5fac2e1d24e88cb204ce858ec52cb3095d
yangqifan97 <qifyang@bu.edu>
2020-01-29 13:36:05 -0500

    change directory & delete NA column

--- join_data.R
+++ join_data.R
@@ -1,11 +1,14 @@
 library(tidyverse)
 
-sellers <- read_csv("Desktop/BA888/brazilian-ecommerce/olist_sellers_dataset.csv")
-items <- read_csv("Desktop/BA888/brazilian-ecommerce/olist_order_items_dataset.csv")
-cd <- read_csv("Desktop/BA888/marketing-funnel-olist/olist_closed_deals_dataset.csv")
-mql <- read_csv("Desktop/BA888/marketing-funnel-olist/olist_marketing_qualified_leads_dataset.csv")
+sellers <- read_csv("olist_sellers_dataset.csv")
+items <- read_csv("olist_order_items_dataset.csv")
+cd <- read_csv("olist_closed_deals_dataset.csv")
+mql <- read_csv("olist_marketing_qualified_leads_dataset.csv")
 
 mf <- merge(cd, mql, by="mql_id")
 BR <- merge(sellers, items, by="seller_id")
-final <- merge(mf, BR, by="seller_id")
+combined <- merge(mf, BR, by="seller_id")
+
+final <- combined %>% 
+  select(-has_company,-has_gtin,-average_stock,-declared_product_catalog_size)
 
